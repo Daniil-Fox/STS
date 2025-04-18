85e1f3852828c1da91330feb7401078e60746eff
Artyushenko Daniil <foxones80@gmail.com>
2025-04-18 19:54:34 +0600

    added smooth corners

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
 import "./_components.js";
 import { burger } from "./functions/burger.js";
+import { renderSquircle } from "corner-smoothing";
+
+document.querySelectorAll(".corner-smooth-50").forEach((el) => {
+  renderSquircle(el, {
+    cornerRadius: 50,
+  });
+});
